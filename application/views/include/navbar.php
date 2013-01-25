6d78bac1cc6c8e2efc010a03ba1c8644c93ae492
Jordon Phillips <phillijk@rose-hulman.edu>
2013-01-25 08:21:47 -0500

    frontpage.php modified to load the header and navbar from the include folder, so we eliminate duplicate code.

--- application/views/include/navbar.php
+++ application/views/include/navbar.php
@@ -37,7 +37,7 @@
           <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
           <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
             <!-- Login form here -->
-            <form action="[YOUR ACTION]" method="post" accept-charset="UTF-8">
+            <form>
               <input id="user_username" style="margin-bottom: 15px;" type="text" name="user[username]" size="30" />
               <input id="user_password" style="margin-bottom: 15px;" type="password" name="user[password]" size="30" />
               <input id="user_remember_me" style="float: left; margin-right: 10px;" type="checkbox" name="user[remember_me]" value="1" />
