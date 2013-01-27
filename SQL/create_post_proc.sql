05423f737c79e2c4150c0711b3a69ac8f4da3ed7
Jack Ma <mac@rose-hulman.edu>
2013-01-27 12:10:39 -0500

    adding create post procedure

---
+++ SQL/create_post_proc.sql
@@ -0,0 +1,16 @@
+-- usage:  call create_post_proc("jack", "some topic", @pid)
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `create_post_proc`(IN `uname` VARCHAR(10), IN `ptopic` VARCHAR(100), OUT `pid` INT)
+BEGIN
+
+DECLARE EXIT HANDLER FOR SQLEXCEPTION ROLLBACK;
+DECLARE EXIT HANDLER FOR SQLWARNING ROLLBACK;
+
+START TRANSACTION;
+
+insert into post(userName, topic) values (uname, ptopic);
+
+SET pid = (select postId from post order by postId desc limit 1);
+COMMIT;
+
+END;
