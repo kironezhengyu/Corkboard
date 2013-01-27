dcc96e03f41f5bd59117d83cc9585c5c96381226
Jordon Phillips <phillijk@rose-hulman.edu>
2013-01-27 10:08:22 -0500

    Login in the navbar works.

--- application/views/include/navbar.php
+++ application/views/include/navbar.php
@@ -37,9 +37,10 @@
           <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
           <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
             <!-- Login form here -->
-            <form>
-              <input id="user_username" style="margin-bottom: 15px;" type="text" name="user[username]" size="30" />
-              <input id="user_password" style="margin-bottom: 15px;" type="password" name="user[password]" size="30" />
+            <?php echo validation_errors(); ?>
+            <?php echo form_open('verifylogin'); ?>
+              <input id="user_username" style="margin-bottom: 15px;" type="text" name="username" size="30" />
+              <input id="user_password" style="margin-bottom: 15px;" type="password" name="password" size="30" />
               <input id="user_remember_me" style="float: left; margin-right: 10px;" type="checkbox" name="user[remember_me]" value="1" />
               <label class="string optional" for="user_remember_me"> Remember me</label>
 
