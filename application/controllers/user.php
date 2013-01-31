fdb1a34c12430d4e759cd968ed584379951637dc
Jordon Phillips <phillijk@rose-hulman.edu>
2013-01-30 23:30:25 -0500

    Improved client-side validation

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -12,10 +12,10 @@ class user extends CI_Controller {
 		$this->load->helper('form');
 		$this->load->library('form_validation');
 		
-		$this->form_validation->set_rules('username', 'User Name', 'required');
-		$this->form_validation->set_rules('nickname', 'Nick Name', 'required');
-		$this->form_validation->set_rules('password', 'Password', 'required');	
-		$this->form_validation->set_rules('conf_password', 'Confirm Password', 'required');	
+		$this->form_validation->set_rules('username', 'User Name', 'trim|required|is_unique[user.userName]|xss_clean');
+		$this->form_validation->set_rules('nickname', 'Nick Name', 'trim|required|is_unique[user.nickname]|xss_clean');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[4]|max_length[100]|xss_clean');	
+		$this->form_validation->set_rules('conf_password', 'Confirm Password', 'trim|required|matches[password]|xss_clean');	
 		
 		if ($this->form_validation->run() === FALSE)
 		{
--- application/views/login_view.php
+++ application/views/login_view.php
@@ -4,7 +4,13 @@
 <div class="container pagination-centered">
 	<div class="span4 offset4">
 		<div class="well">
-			<?php echo validation_errors(); ?>
+			<?php if(validation_errors()):?>
+				<div class="alert alert-error">
+					<button type="button" class="close" data-dismiss="alert">&times;</button>
+					<?php echo validation_errors(); ?>
+				</div>
+			<?php endif;?>
+			
 			<?php echo form_open('verifylogin'); ?>
 			  <label for="username">Username:</label>
 			  <input type="text" size="20" id="username" name="username"/>
