67184e2f9c7ec06ff80249a1ad664056360243a3
Jordon Phillips <phillijk@rose-hulman.edu>
2013-01-30 21:12:54 -0500

    increased the size of the username to account for the size of MD5

--- SQL/Tables.sql
+++ SQL/Tables.sql
@@ -1,11 +1,11 @@
 CREATE TABLE user (
-         userName VARCHAR(10) NOT NULL,
+         userName VARCHAR(100) NOT NULL,
          PRIMARY KEY(userName),
          nickname VARCHAR(100) NOT NULL,
          password VARCHAR(100) NOT NULL
        );
 CREATE TABLE post(
-       userName VARCHAR(10) NOT NULL,
+       userName VARCHAR(100) NOT NULL,
        FOREIGN KEY (userName) REFERENCES user(userName),
        postId INT(30) NOT NULL AUTO_INCREMENT,
 	   PRIMARY KEY(postId),
@@ -19,26 +19,26 @@ CREATE TABLE message(
        PRIMARY KEY(messageId),
        content TEXT ,
        ts TIMESTAMP NOT NULL,
-       userName VARCHAR(10) NOT NULL,
+       userName VARCHAR(100) NOT NULL,
        FOREIGN KEY (userName) REFERENCES user(userName)
 );
 
 CREATE TABLE likes (
         messageId INT(30) NOT NULL,
-        userName VARCHAR(10) NOT NULL,
+        userName VARCHAR(100) NOT NULL,
         PRIMARY KEY (messageId, userName),
         FOREIGN KEY (messageId) REFERENCES message(messageId),
         FOREIGN KEY (userName) REFERENCES user(userName)
 );
 CREATE TABLE friends (
-        userName VARCHAR(10) NOT NULL,
+        userName VARCHAR(100) NOT NULL,
         friend_user_name VARCHAR(10) NOT NULL,
         PRIMARY KEY (userName, friend_user_name),
         FOREIGN KEY (userName) REFERENCES user(userName),
         FOREIGN KEY (friend_user_name) REFERENCES user(userName)
 );
 CREATE TABLE board_post (
-       userName VARCHAR(10) NOT NULL,
+       userName VARCHAR(100) NOT NULL,
         postId INT(30) NOT NULL,
         PRIMARY KEY (userName, PostId),
         FOREIGN KEY (userName) REFERENCES user(userName),
