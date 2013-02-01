0ef95b05a44e28af439a910f40364c6a897fe2c2
Jordon Phillips <phillijk@rose-hulman.edu>
2013-01-31 19:29:03 -0500

    Added procedure for fetching the most recent posts from a given user.

--- SQL/fetch_post_proc.sql
+++ SQL/fetch_post_proc.sql
@@ -1,8 +1,15 @@
 DELIMITER $$
-CREATE PROCEDURE fetch_post_proc(IN pid int(30))
+CREATE PROCEDURE fetch_post_proc(IN uname VARCHAR(100), IN latest_offset MEDIUMINT)
 BEGIN
+	DECLARE pid INT(30);
+	SET pid = (SELECT postId
+	FROM post
+	WHERE userName = uname
+	ORDER BY postId DESC
+	LIMIT 1 OFFSET latest_offset);
+
 	SELECT *
 	FROM post_view
-	WHERE post_view.postId = pid;
+	WHERE postId = pid;
 END $$
 DELIMITER ;
