8e1e9c526901579b54a847d2e3db8a08e131dbd7
Jack Ma <mac@rose-hulman.edu>
2013-02-10 02:51:44 -0500

    fetch post proc update

--- SQL/fetch_post_proc.sql
+++ SQL/fetch_post_proc.sql
@@ -1,18 +1,6 @@
 DELIMITER $$
 CREATE PROCEDURE fetch_post_proc(IN uname VARCHAR(100), IN latest_offset MEDIUMINT, IN fetch_amt MEDIUMINT)
 BEGIN
-	IF uname IS NULL
-	THEN
-		SIGNAL SQLSTATE '02000'
-			SET MESSAGE_TEXT = 'Values may not be NULL.';
-	END IF;
-	
-	IF uname NOT EXISTS IN (SELECT userName FROM user WHERE userName = uname)
-	THEN
-		SIGNAL SQLSTATE '05000'
-			SET MESSAGE_TEXT = 'Username does not exist.';
-	END IF;
-
 	SELECT *
 	FROM post_view pv JOIN (SELECT postId FROM post
 							WHERE userName = uname
