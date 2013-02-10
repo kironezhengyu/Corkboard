fd484201f4c9cba1c99587e0151f1daa3281dcbe
Jack Ma <mac@rose-hulman.edu>
2013-02-09 23:16:25 -0500

    post_view now includes like

--- SQL/post_view.sql
+++ SQL/post_view.sql
@@ -1,4 +1,4 @@
 CREATE VIEW `corkboard`.`post_view` AS
-SELECT post.postId, post.topic, messageId, message.userName AS user_commenting, post.userName AS user_op, nickname, ts, content
-FROM post, message, user
-WHERE post.postId = message.postId AND message.userName = user.username;
+SELECT post.postId, post.topic, messageId, message.userName AS user_commenting, post.userName AS user_op, nickname, message.ts, content, num_likes-1 as num_likes
+FROM post, message, user, post_likes
+WHERE post.postId = message.postId AND message.userName = user.username AND post_likes.postId=post.postId;
